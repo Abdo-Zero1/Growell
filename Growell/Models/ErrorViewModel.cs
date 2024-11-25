3bdee1d388058ee7c4bb60db39e69a163047d21e
abdoelsayedDragon1 <abdo.com.a777@gmail.com>
2024-11-25 14:36:43 +0200

    Add project files.

---
+++ Growell/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Growell.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
